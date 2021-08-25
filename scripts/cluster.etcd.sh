63ac93e8b22c87f48f666d595adf41d1935913d0
k32 <10274441+k32@users.noreply.github.com>
2021-08-25 17:12:50 +0200

    chore(rename): Remove extra scripts

--- scripts/cluster.etcd.sh
+++
@@ -1,4 +0,0 @@
-
-erl -pa ebin/ -pa deps/*/ebin -name mria1@127.0.0.1 -setcookie cookie -config data/app.etcd -s mria -s mria autocluster
-
-erl -pa ebin/ -pa deps/*/ebin -name mria2@127.0.0.1 -setcookie cookie -config data/app.etcd -s mria -s mria autocluster
