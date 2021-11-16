e92fb4652439b0ea5fab5cfa17a7b2e6b0339f7e
k32 <10274441+k32@users.noreply.github.com>
2021-11-17 00:09:29 +0100

    test(slowdown.sh): Add more knobs to netem

--- test/mria_mnesia_SUITE_data/cluster_benchmark/slowdown.sh
+++ test/mria_mnesia_SUITE_data/cluster_benchmark/slowdown.sh
@@ -9,9 +9,9 @@ set -euo pipefail
 [ -z ${1+1} ] && {
     echo "Emulate network latency on the localhost.
 
-Usage:
+USAGE:
 
-    $(basename $0) DELAY JITTER [PORT1 PORT2 ...]
+    $(basename $0) [ -d DELAY ] [ -j JITTER ] [ -r RATE ] [PORT1 PORT2 ...]
 
 It is possible to specify PORT as 'empd' to apply delay to the
 distribution ports of all running BEAM VMs (excluding the CT
@@ -19,9 +19,24 @@ master).
 
 Both DELAY and JITTER should be more than 0
 
-Example:
+Port can be:
 
-    $(basename $0) 500ms 1 8001 8002 empd"
+   1. A number: it will apply netem on the messages that are sent to
+      and from the port
+
+   2. A number with 'd' prefix (e.g. d1883): it will apply netem on
+      the messages sent to the port
+
+   3. A number with 's' prefix (e.g. s1883): it will apply netem on
+      the messages sent from the port
+
+   4. 'epmd': It will apply delay to all ports registered in epmd and
+      apply netem to all erlang distribution protocol connections.
+
+EXAMPLE:
+
+    $(basename $0) -d 500ms -j 1 -r 10kbps 8001 s8002 empd
+"
     exit 1;
 }
 
@@ -45,7 +60,7 @@ CHAIN="OUTPUT"
 iptables -t mangle -F "$CHAIN" || true
 tc qdisc del dev "$INTERFACE" root || true
 
-echo "Delay=${DELAY} jitter=${JITTER} interface=${INTERFACE}"
+echo "Delay=${DELAY} jitter=${JITTER} rate=${RATE} interface=${INTERFACE}"
 
 # Shape packets marked as 12
 MARK=12
@@ -56,11 +71,24 @@ tc class add dev "$INTERFACE" parent 1: classid 1:$ID htb rate "$RATE"
 tc qdisc add dev "$INTERFACE" parent 1:$ID handle $MARK netem delay $DELAY $JITTER distribution normal
 tc filter add dev "$INTERFACE" parent 1: prio 1 protocol ip handle $MARK fw flowid 1:$ID
 
+mark() {
+    echo "Applying netem on $1 $2"
+    iptables -A "${CHAIN}" -p tcp -t mangle -j MARK --set-mark $MARK $1 $2
+}
+
 # Create firewall rules to mark the packets:
 mark_port() {
-    PORT=$1
-    echo "Adding latency on tcp port $PORT"
-    iptables -A "${CHAIN}" -p tcp --dport $PORT -t mangle -j MARK --set-mark $MARK
+    local PORT=$1
+    if [[ $PORT =~ ^([0-9]+)$ ]]; then
+        mark --sport $PORT
+        mark --dport $PORT
+    elif [[ $PORT =~ ^s([0-9]+)$ ]]; then
+        PORT=${BASH_REMATCH[1]}
+        mark --sport $PORT
+    elif [[ $PORT =~ ^d([0-9]+)$ ]]; then
+        PORT=${BASH_REMATCH[1]}
+        mark --dport $PORT
+    fi
 }
 
 while [ ! -z ${1+1} ]; do
