27e6b55f01cc40949f7397bfc65b76eac392ce55
Thales Macedo Garitezi <thalesmg@gmail.com>
2022-02-16 13:17:22 -0300

    test: improve test failure comment

--- test/mria_proper_utils.erl
+++ test/mria_proper_utils.erl
@@ -130,8 +130,8 @@ check_state(Cmds, #s{bag = Bag, set = Set}, Node) ->
     compare_lists(set, Node, Cmds, lists:sort(maps:to_list(Set)), get_records(Node, test_tab)).
 
 compare_lists(Type, Node, Cmds, Expected, Got) ->
-    Unexpected = Expected -- Got,
-    Missing = Got -- Expected,
+    Missing = Expected -- Got,
+    Unexpected = Got -- Expected,
     Comment = [ {node, Node}
               , {cmds, Cmds}
               , {unexpected, Unexpected}
